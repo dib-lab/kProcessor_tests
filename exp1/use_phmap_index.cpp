63a4e71db980c364a3d346922bf8b0de8f2ccbf2
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-03-24 13:59:43 +0200

    exp1 cpp code

---
+++ exp1/use_phmap_index.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <kDataFrame.hpp>
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithms.hpp>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    string index_prefix = "../idx_gencode.v33.pc_transcripts";
+    string fasta_file = "../gencode.v33.pc_transcripts.fa";
+    string names_file = fasta_file + ".names";
+    int chunkSize = 1000;
+    int kmerSize = 31;
+
+    // Indexing
+
+    kmerDecoder *KMERS = kProcessor::initialize_kmerDecoder(fasta_file, chunkSize, "kmers", {{"k_size", kmerSize}});
+    kDataFrame *KF = new kDataFramePHMAP(kmerSize, 1);
+    colored_kDataFrame* ckf= kProcessor::index(KMERS, names_file, KF);
+
+    // Dumping
+    // ckf->save("idx_phmap_new");
+
+}
